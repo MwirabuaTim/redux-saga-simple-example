ea3412419a0a0b1222da0a05913e77c7936ef291
TechyTimo <techytimo@gmail.com>
2018-03-21 10:01:12 +0300

    simplified

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
+	
 
-import Main from './Main';
+class App extends Component {
+  render() {
+    return (
+      <div style={{width: '40%', marginLeft: '30%', marginRight: '30%', paddingTop: 100}}>
+        <h1>Weather</h1>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    )
+  }
+}
 
 function mapStateToProps(state) {
   return {
@@ -15,4 +26,4 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -2,6 +2,7 @@ import { takeEvery } from 'redux-saga';
 import { fork, call, put } from 'redux-saga/effects';
 import request from 'superagent';
 
+// Worker
 function getWeather(location) {
   const url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D"${location}")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 
@@ -10,6 +11,7 @@ function getWeather(location) {
     .then(data => JSON.parse(data.text));
 }
 
+// Worker
 function* callGetWeather({location, resolve, reject}) {
   const result = yield call(getWeather, location);
   console.log(result.query);
@@ -22,6 +24,7 @@ function* callGetWeather({location, resolve, reject}) {
   }
 }
 
+// Watcher
 function* getWeatherSaga() {
   yield* takeEvery('FETCH_WEATHER', callGetWeather);
 }
